176357ddeeb323db70d8d769c4b76fe26206bcda
Munkh-Od Jargalsaikhan <munhod@gmail.com>
2017-06-24 16:30:05 +0800

    keys are added to gitignore + some code udpates

--- twitter_sentiment_analysis.py
+++ twitter_sentiment_analysis.py
@@ -1,11 +1,18 @@
 import tweepy
-from textblob import textblob
+from textblob import TextBlob
+from keys import consumer_key, consumer_secret, access_token, access_token_secret
 
-consumer_key = ""
-consumer_secret = ""
 
-access_token = ""
-access_token_secret = ""
-
-auth = tweepy.OAuthhandler(consumer_key, consumer_secret)
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search('Putin', count=100)
+
+for tweet in public_tweets:
+  print(tweet.text)
+  analysis = TextBlob(tweet.text)
+  print(analysis.sentiment)
+
+print(TextBlob("I didn't like him. His coffee was awful. I dont want to see him again").sentiment)
